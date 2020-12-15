72080e9eab6e3b56d86f837b3fa344bc46f7beb8
JuantonioMS <b32masaj@gmail.com>
2020-12-15 16:49:08 +0100

    Outfiles statement

---
+++ dummylearning/outfiles/clasification.py
@@ -0,0 +1,57 @@
+from dummylearning.outfiles.base import OutfileBase
+from dummylearning.analysis.clasification import Analysis
+
+class Outfile(OutfileBase):
+
+    def __init__(self, model, verbose = True):
+        super().__init__(verbose)
+        self.model = model
+        self.analysis = Analysis(self.model)
+
+
+
+
+    def coefficients(self, outfile):
+        coefs = self.analysis.coefficients()
+
+        with open(f"{outfile}.csv", "w") as file:
+            row = "Coefficients\t" + "\t".join([clas for clas in coefs]) + "\n"
+            file.write(row)
+            for coef in coefs[list(coefs.keys())[0]].keys():
+                row = f"{coef}\t" + "\t".join([str(coefs[clas][coef]) for clas in coefs]) + "\n"
+                file.write(row)
+
+
+
+
+    def oddsRatios(self, outfile):
+        odds = self.analysis.oddsRatios()
+
+        with open(f"{outfile}.csv", "w") as file:
+            row = "Odds Ratios\t" + "\t".join([clas for clas in odds]) + "\n"
+            file.write(row)
+            for odd in odds[list(odds.keys())[0]].keys():
+                row = f"{odd}\t" + "\t".join([str(odds[clas][odd]) for clas in odds]) + "\n"
+                file.write(row)
+
+
+
+
+    def log2oddsRatios(self, outfile):
+        log2odds = self.analysis.log2oddsRatios()
+
+        with open(f"{outfile}.csv", "w") as file:
+            row = "Log2 Odds Ratios\t" + "\t".join([clas for clas in log2odds]) + "\n"
+            file.write(row)
+            for log2odd in log2odds[list(log2odds.keys())[0]].keys():
+                row = f"{log2odd}\t" + "\t".join([str(log2odds[clas][log2odd]) for clas in log2odds]) + "\n"
+                file.write(row)
+
+
+
+
+    def rocInfo(self, outfile):
+        pass
+
+    def prcInfo(self, outfile):
+        pass
